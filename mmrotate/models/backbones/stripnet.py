bdd9801015ab7b373e4c229dd62c96b0abf4c78a
unknown <1198983825@qq.com>
2025-01-27 13:54:44 +0800

    modify stripnet.py

--- mmrotate/models/backbones/stripnet.py
+++ mmrotate/models/backbones/stripnet.py
@@ -35,15 +35,15 @@ class StripBlock(nn.Module):
     def __init__(self, dim, k1, k2):
         super().__init__()
         self.conv0 = nn.Conv2d(dim, dim, 5, padding=2, groups=dim)
-        self.strip_conv1 = nn.Conv2d(dim,dim,kernel_size=(k1, k2), stride=1, padding=(k1//2, k2//2), groups=dim)     
-        self.strip_conv2 = nn.Conv2d(dim,dim,kernel_size=(k2, k1), stride=1, padding=(k2//2, k1//2), groups=dim)
+        self.conv_spatial1 = nn.Conv2d(dim,dim,kernel_size=(k1, k2), stride=1, padding=(k1//2, k2//2), groups=dim)     
+        self.conv_spatial2 = nn.Conv2d(dim,dim,kernel_size=(k2, k1), stride=1, padding=(k2//2, k1//2), groups=dim)
 
         self.conv1 = nn.Conv2d(dim, dim, 1)
 
     def forward(self, x):   
         attn = self.conv0(x)
-        attn = self.strip_conv1(attn)
-        attn = self.strip_conv2(attn)
+        attn = self.conv_spatial1(attn)
+        attn = self.conv_spatial2(attn)
         attn = self.conv1(attn)
 
         return x * attn
